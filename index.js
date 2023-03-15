6122b8f89383f97de6a5212c9150bb1dd9c5de30
Penelope Drago <prd@drago.com.au>
2023-03-15 14:32:53 +1100

    add variables

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+// Multiply variable
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+// Random variable
+const random = Math.ceil(Math.random());
+
+// Mod variable
+const num3 = 22;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+// Max variable
+const max = Math.max(2, 8, 14, 20);
